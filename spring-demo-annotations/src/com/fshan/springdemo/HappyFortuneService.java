5718220ef4a5989662f926771d0c0c1ee0160815
Shan Fuyang <fuyang@Shans-MBP.attlocal.net>
2019-10-30 11:20:10 -0700

    Learning Annotation

---
+++ spring-demo-annotations/src/com/fshan/springdemo/HappyFortuneService.java
@@ -0,0 +1,14 @@
+package com.fshan.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HappyFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		
+		return "Learning annotation today !!! Yeah!!!";
+	}
+
+}
